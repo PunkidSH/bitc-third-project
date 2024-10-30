03e45fba7a6d994b7735e27765d5b4f1ac716ef7
PunkidSH <940809psh@naver.com>
2024-10-30 11:05:16 +0900

    안드로이드 탄력적 ip로 수정

--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/config/FirebaseConfig.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/config/FirebaseConfig.java
@@ -17,7 +17,7 @@ public class FirebaseConfig {
     // FirebaseApp이 이미 초기화되어 있는지 확인
     if (FirebaseApp.getApps().isEmpty()) {
       try {
-        FileInputStream serviceAccount = new FileInputStream("src/main/bitcfinalprojectkotlin-firebase-adminsdk-p8s27-ddddcaf3aa.json");
+        FileInputStream serviceAccount = new FileInputStream("/home/ec2-user/Firebase/bitc-final-prj-push-alarm-firebase-adminsdk-n3l5p-71ef4cd499.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build();
--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/config/WebMvcConfig.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package bitc.fullstack405.finalprojectspringboot.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -11,17 +13,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry reg) {
     reg.addMapping("/**")
-        .allowedOrigins("http://localhost:5173")
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*");
   }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     // 지정한 외부폴더를 스프링의 내부 폴더로 추가(행사 이미지)
-    registry.addResourceHandler("/eventImg/**").addResourceLocations("file:../eventImg/");
+    registry.addResourceHandler("/eventImg/**").addResourceLocations("file:/home/ec2user/eventImg/");
 
     // 지정한 외부폴더를 스프링의 내부 폴더로 추가(QR 코드 이미지)
-    registry.addResourceHandler("/qrImg/**").addResourceLocations("file:../qrImg/");
+    registry.addResourceHandler("/qrImg/**").addResourceLocations("file:/home/ec2-user/qrImg/");
+
+    registry.addResourceHandler("/assets/**")
+        .addResourceLocations("classpath:/static/assets/")
+        .setCachePeriod(3600)
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver());
   }
 }
